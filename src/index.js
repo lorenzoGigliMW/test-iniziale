789c3ff4b1e9d4f79be95207940fbd3238a85f5c
lorenzoGigliMW <lorenzo.gigli@marketwall.com>
2023-02-24 11:51:16 +0100

    event1

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const DATA = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App task={DATA} />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
