940a74e60d456e845a7c460ea77b34bb4bf67cbf
lorenzoGigliMW <lorenzo.gigli@marketwall.com>
2023-02-24 17:06:00 +0100

    PrimaAppCompleta

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -14,6 +21,9 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
+
+
+
 const [filter, setFilter] = useState('All');
 const filterList = FILTER_NAMES.map((name) => (
   <FilterButton
@@ -72,8 +82,18 @@ const filterList = FILTER_NAMES.map((name) => (
   />
 )
 );
+
 const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
 const headingText = `${taskList.length} ${tasksNoun} remaining`;
+const listHeadingRef = useRef(null);
+
+const prevTaskLength = usePrevious(tasks.length);   //Gestione focus da tastiera dopo delete
+useEffect(() => {                                    //
+  if (tasks.length - prevTaskLength === -1) {        //
+    listHeadingRef.current.focus();
+  }
+}, [tasks.length, prevTaskLength]);                   //
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -81,7 +101,9 @@ const headingText = `${taskList.length} ${tasksNoun} remaining`;
       <div className="filters btn-group stack-exception">
       {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
 export default function Todo(props) {
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -14,7 +28,7 @@ export default function Todo(props) {
     setEditing(false);
   }
 
-  const editingTemplate = (
+  const editingTemplate = (                                   //vista di modifica
     <form className="stack-small" onSubmit={handleSubmit}>
       <div className="form-group">
         <label className="todo-label" htmlFor={props.id}>
@@ -26,6 +40,7 @@ export default function Todo(props) {
           type="text"
           value={newName}
           onChange={handleChange}
+          ref={editFieldRef}                //riferimento per utilizzo da tastiera
         />
       </div>
       <div className="btn-group">
@@ -44,7 +59,7 @@ export default function Todo(props) {
       </div>
     </form>
   );
-  const viewTemplate = (
+  const viewTemplate = (                                      //vista base con impl eliminazione task
     <div className="stack-small">
       <div className="c-cb">
           <input
@@ -56,9 +71,10 @@ export default function Todo(props) {
           <label className="todo-label" htmlFor={props.id}>
             {props.name}
           </label>
-        </div>
+        </div>                               
         <div className="btn-group">
-        <button type="button" className="btn" onClick={() => setEditing(true)}>
+        <button type="button" className="btn" onClick={() => setEditing(true)} ref={editButtonRef} //riferimento per utilizzo da tastiera
+        >  
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
           <button
@@ -71,5 +87,13 @@ export default function Todo(props) {
         </div>
     </div>
   );
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
     return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
   }
