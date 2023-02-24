a729656dd64febed87142f8eb2a96c685ca0a22a
lorenzoGigliMW <lorenzo.gigli@marketwall.com>
2023-02-24 13:11:00 +0100

    eventiFatti

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { nanoid } from "nanoid";
 
-function addTask(name) {
-    alert(name);
-  }
+
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+    const [tasks, setTasks] = useState(props.tasks);
+    
+    function addTask(name) {
+      const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+  setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const taskList = tasks.map((task) => (
     <Todo
     id={task.id}
     name={task.name}
     completed={task.completed}
     key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
   />
 )
 );
+const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -25,7 +54,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
